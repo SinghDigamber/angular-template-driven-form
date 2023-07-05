f11257ab8b04c6c6fb0c8f8631fd3e37c0f57d6b
remotestack377 <digamber@positronx.io>
2023-07-05 20:40:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+export class User {
+  public name!: string;
+  public email!: string;
+  public password!: string;
+  public hobbies!: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-template-driven-form';
+  model = new User();
+
+  Hobbies: string[] = [
+    'Acrobatics',
+    'Acting',
+    'Animation',
+    'Astronomy',
+    'Baking',
+  ];
+
+  constructor() {}
+
+  onSubmit(form: any) {
+    console.log(form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
